f21236f6ddb57580d6baae42c2c77c93a8895175
slrahmd <xdkoolsalaar@gmail.com>
2022-01-01 16:40:55 +0300

    Used MUI SendIcon, Centered InputLabel

--- src/Components/MessageComp.js
+++ src/Components/MessageComp.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Box } from '@material-ui/core'
+import { margin } from '@mui/system'
 
 const MessageComp = ({text}) => {
     return (
         <div>
-            {text}
+            <Box component="span" 
+            sx={{ display: 'block', minHeight: '2.5rem', bgcolor: '#1B7DFF',
+            borderRadius: '10px', boxShadow: '5', }}>
+                {text}
+            </Box>
+            
         </div>
     )
 }
--- src/Components/Messenger.js
+++ src/Components/Messenger.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import MessageComp from './MessageComp';
-import { Button, FormControl, Input, InputLabel } from '@material-ui/core'
+import {Typography, FormControl, Input, InputLabel, IconButton} from '@material-ui/core'
 import SignOut from './SignOut';
-import {auth, db} from '../firebase'
+import { Send } from '@mui/icons-material';
+import { auth, db } from '../firebase'
 import firebase from 'firebase/compat/app';
+import './component.css';
 
 const Messenger = () => {
 
     const [input, setInput] = useState("");
     const [messages, setMessages] = useState([]);
 
-    useEffect(()=> {db.collection('Messages').orderBy('createdAt').limit(50)
-    .onSnapshot(snapshot => {
-      setMessages(snapshot.docs.map(doc => doc.data()))
-    })}
-    ,[]);
+    useEffect(() => {
+        db.collection('Messages').orderBy('createdAt').limit(50)
+        .onSnapshot(snapshot => {
+            setMessages(snapshot.docs.map(doc => doc.data()))
+        })
+    }
+        , []);
 
     async function onSubmit(e) {
         e.preventDefault();
 
-        const{uid, photoURL} = auth.currentUser;
+        const { uid, photoURL } = auth.currentUser;
 
         await db.collection('Messages').add({
             message: input,
@@ -31,23 +35,30 @@ const Messenger = () => {
         setInput('');
     }
 
-        
+
     return (
         <div>
-            <SignOut/>
-            <form onSubmit={onSubmit}>
-                <FormControl>
-                    <InputLabel htmlFor="my-input">Type Message...</InputLabel>
-                    <Input value={input} onChange={(e) => setInput(e.target.value)} aria-describedby="my-helper-text" />
-                    <Button variant="contained" type='Submit'>Send</Button>
-                </FormControl>
-                
-            </form>
-        {
-        messages.map(
-        (message) => ( <MessageComp key={message.id} text={message.message} />)
-        )
-        }
+            <SignOut />
+            <div className="messages">
+                {
+                    messages.map(
+                        (message) => (<MessageComp key={message.id} text={message.message} />)
+                    )
+                }
+            </div>
+            <div className='inputComponent'>
+                <form onSubmit={onSubmit}>
+                    <FormControl style={{ display: 'flex', justifyContent: 'center', 
+                    alignItems: 'center'}}>
+                        <InputLabel style={{left:'auto', margin:'auto'}} htmlFor="my-input">Type Message...</InputLabel>
+                        <Input value={input} onChange={(e) => setInput(e.target.value)} aria-describedby="my-helper-text" />
+                        <IconButton disabled={!input} variant="contained" type='Submit' color="primary" >
+                            <Send/>
+                        </IconButton>
+                    </FormControl>
+                </form>
+
+            </div>
         </div>
     )
 }
